4ea62b3c466ad3dfcabfd000ed1e411985b3aeef
AnthSuz <56254134+AnthSuz@users.noreply.github.com>
2024-06-24 14:59:19 +0200

    Clear App and Use CSV Reader

--- src/App.js
+++ src/App.js
@@ -1,23 +1,134 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { useCSVReader, lightenDarkenColor } from "react-papaparse";
+
+const GREY = "#CCC";
+const GREY_LIGHT = "rgba(255, 255, 255, 0.4)";
+const DEFAULT_REMOVE_HOVER_COLOR = "#A01919";
+const REMOVE_HOVER_COLOR_LIGHT = lightenDarkenColor(
+  DEFAULT_REMOVE_HOVER_COLOR,
+  40
+);
+const GREY_DIM = "#686868";
+
+const styles = {
+  zone: {
+    alignItems: "center",
+    border: `2px dashed ${GREY}`,
+    borderRadius: 20,
+    display: "flex",
+    flexDirection: "column",
+    height: "100%",
+    justifyContent: "center",
+    padding: 60,
+    cursor: "pointer",
+  },
+  file: {
+    background: "#C2C2C2",
+    borderRadius: 20,
+    display: "flex",
+    height: 150,
+    width: 150,
+    position: "relative",
+    zIndex: 10,
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+  info: {
+    alignItems: "center",
+    display: "flex",
+    flexDirection: "column",
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  name: {
+    backgroundColor: GREY_LIGHT,
+    borderRadius: 3,
+    fontSize: 12,
+    marginBottom: "0.5em",
+  },
+  zoneHover: {
+    borderColor: { GREY },
+  },
+  default: {
+    borderColor: { GREY_DIM },
+  },
+  remove: {
+    height: 23,
+    position: "absolute",
+    right: 6,
+    top: 6,
+    width: 23,
+  },
+};
 
 function App() {
+  const { CSVReader } = useCSVReader();
+  const [zoneHover, setZoneHover] = useState(false);
+  const [data, setData] = useState([]);
+  const [removeHoverColor, setRemoveHoverColor] = useState(
+    DEFAULT_REMOVE_HOVER_COLOR
+  );
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CSVReader
+        config={{
+          header: true,
+        }}
+        onUploadAccepted={(results) => {
+          setData(
+            JSON.parse(JSON.stringify(results.data).replace(/[\uFFFD]/g, "é"))
+          );
+          setZoneHover(false);
+        }}
+        onDragOver={(event) => {
+          event.preventDefault();
+          setZoneHover(true);
+        }}
+        onDragLeave={(event) => {
+          event.preventDefault();
+          setZoneHover(false);
+        }}
+      >
+        {({ getRootProps, acceptedFile, getRemoveFileProps, Remove }) => (
+          <>
+            <div
+              {...getRootProps()}
+              style={Object.assign(
+                {},
+                styles.zone,
+                zoneHover && styles.zoneHover
+              )}
+            >
+              {acceptedFile ? (
+                <>
+                  <div style={styles.file}>
+                    <div style={styles.info}>
+                      <span style={styles.name}>{acceptedFile.name}</span>
+                    </div>
+                    <div
+                      {...getRemoveFileProps()}
+                      style={styles.remove}
+                      onMouseOver={(event) => {
+                        event.preventDefault();
+                        setRemoveHoverColor(REMOVE_HOVER_COLOR_LIGHT);
+                      }}
+                      onMouseOut={(event) => {
+                        event.preventDefault();
+                        setRemoveHoverColor(DEFAULT_REMOVE_HOVER_COLOR);
+                      }}
+                    >
+                      <Remove color={removeHoverColor} />
+                    </div>
+                  </div>
+                </>
+              ) : (
+                "Drop CSV file here or click to upload"
+              )}
+            </div>
+          </>
+        )}
+      </CSVReader>
     </div>
   );
 }
